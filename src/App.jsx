9d44997496f7f2a827cf3612b6eedf59b8d143f7
M3hTi <mehdinasir502@yahoo.com>
2025-03-25 12:23:52 +0330

    feat: implement shopping cart functionality in App component with add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import Body from "./Body";
 import { useProducts } from "./hooks/useProducts";
 
 import "./App.css";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const { products, isLoading, error } = useProducts();
+  const [shoppingCart, setShoppingCart] = useState([]);
+
+  function handleAddToCart(item) {
+    console.log(item)
+    setShoppingCart((s) => [...s, item]);
+  }
+
   return (
     <>
-      <Header />
+      <Header cartItems={shoppingCart} />
 
-      <Body products={products} isLoading={isLoading} error={error} />
+      <Body
+        products={products}
+        isLoading={isLoading}
+        error={error}
+        onAdd={handleAddToCart}
+      />
     </>
   );
 }
