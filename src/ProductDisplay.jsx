0bc9ebd3eb3bfb7bb1f7d78a15d8c5d95536db4d
M3hTi <mehdinasir502@yahoo.com>
2025-03-25 11:53:08 +0330

    fix: add optional chaining to products mapping in ProductDisplay component

--- src/ProductDisplay.jsx
+++ src/ProductDisplay.jsx
@@ -2,7 +2,7 @@ import Product from "./Product";
 function ProductDisplay({ products }) {
   return (
     <section className="products-grid">
-      {products.map((product) => (
+      {products?.map((product) => (
         <Product key={product.id} productObj={product} />
       ))}
     </section>
