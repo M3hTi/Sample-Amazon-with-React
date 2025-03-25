79e6156452a5a708b2b9525541322a77402284d3
M3hTi <mehdinasir502@yahoo.com>
2025-03-25 12:50:25 +0330

    feat: implement product search functionality in App component with query state and filtering

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,24 @@ import { useEffect, useState } from "react";
 function App() {
   const { products, isLoading, error } = useProducts();
   const [shoppingCart, setShoppingCart] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const matchProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(query.toLowerCase())
+  );
+  // console.log(matchProducts);
 
   function handleAddToCart(item) {
-    console.log(item)
+    // console.log(item);
     setShoppingCart((s) => [...s, item]);
   }
 
   return (
     <>
-      <Header cartItems={shoppingCart} />
+      <Header cartItems={shoppingCart} query={query} setQuery={setQuery} />
 
       <Body
-        products={products}
+        products={matchProducts}
         isLoading={isLoading}
         error={error}
         onAdd={handleAddToCart}
