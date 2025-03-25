35388b8bbd915baa894b0495b7d29a9875566855
M3hTi <mehdinasir502@yahoo.com>
2025-03-25 11:50:59 +0330

    feat: enhance Body component to handle loading state and errors, integrating Loader, ProductDisplay, and Error components

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,17 @@
 import Carousel from "./Carousel";
+import ProductDisplay from "./ProductDisplay";
+import Loader from "./Loader";
+import Error from "./Error";
 
-function Body() {
+function Body({ products, isLoading, error }) {
   return (
     <div className="app">
       <Carousel />
+      {isLoading && <Loader />}
+      {!isLoading && products.length > 0 && (
+        <ProductDisplay products={products} />
+      )}
+      {error && <Error msg={error} />}
     </div>
   );
 }
