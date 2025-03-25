db823ccbecbb7174176ab086e094868f250eb8c0
M3hTi <mehdinasir502@yahoo.com>
2025-03-25 14:58:51 +0330

    feat: enhance Icon component in Header to toggle cart visibility and display cart items or empty message

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,9 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+
+import ShowCartItems from "./ShowCartItems";
 
 function Header({ cartItems, query, setQuery }) {
   return (
@@ -34,7 +37,7 @@ function Header({ cartItems, query, setQuery }) {
           </a>
         </NavItem>
         <div className="cart">
-          <Icon icon={<FaShoppingCart />} />
+          <Icon icon={<FaShoppingCart />} cartItems={cartItems} />
           <span className="cart-count">{cartItems.length}</span>
         </div>
       </div>
@@ -52,8 +55,33 @@ function Logo() {
   );
 }
 
-export function Icon({ icon = null }) {
-  return <span style={{ cursor: "pointer" }}>{icon}</span>;
+export function Icon({ icon = null, cartItems = null }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <span style={{ cursor: "pointer" }} onClick={() => setIsOpen((s) => !s)}>
+        {icon}
+      </span>
+      {isOpen && cartItems?.length > 0 && (
+        <ShowCartItems cartItems={cartItems} />
+      )}
+      {isOpen && cartItems?.length === 0 && (
+        <div className="cart-container">
+          <p
+            style={{
+              color: "black",
+              fontSize: "14px",
+              backgroundColor: "rgb(255, 206, 18)",
+              padding: "10px 8px",
+              borderRadius: "15px",
+            }}
+          >
+            Your Amazon Cart is empty
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
 
 function NavItem({ children, className = null }) {
@@ -70,7 +98,7 @@ function Search({ query, setQuery }) {
         onChange={(e) => setQuery(e.target.value)}
       />
       <button>
-        <Icon icon={<FaSearch />} />
+        <FaSearch />
       </button>
     </>
   );
